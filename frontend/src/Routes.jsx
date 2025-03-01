63a4b719fdf347c48ead05bdbdb5f7d95f93f0bd
reva <reva.raspaile.s73@kalvium.community>
2025-03-01 15:43:18 +0530

    M20 done

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -4,5 +4,6 @@ import Home from './pages/Home'
 import CreateProduct from './pages/createProduct';
 import MyProducts from './pages/myProduct';
 import Cart from './pages/cart'
+import Profile from "./pages/profile"
 import ProductDetails from './pages/productDetails'
-export {LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart,ProductDetails};
+export {LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart,ProductDetails,Profile};
